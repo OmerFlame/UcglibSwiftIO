2071f7a923e55ce415314824e08a9dce7a1327a8
Omer Shamai <renginald.bagel@gmail.com>
2022-10-24 19:51:49 +0300

    import CUcglibSwiftIO

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Ucglib",
-            targets: ["UcglibSwiftIO"]
+            targets: ["UcglibSwiftIO", "CUcglibSwiftIO"]
         )
     ],
     dependencies: [
